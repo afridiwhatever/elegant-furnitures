c5ee2a76016557ad8292bb73100eb02c89787981
Afridi <afridiwhatever@gmail.com>
2024-04-30 18:04:49 +0600

    working on sale section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,17 +5,33 @@ import About from "@/components/About";
 import Categories from "@/components/Categories";
 import NewArrivals from "@/components/NewArrivals";
 import Features from "@/components/Features";
+import ShopNowButton from "@/components/ShopNowButton";
+import Image from "next/image";
 
 const HomePage = () => {
   return (
     <>
-      <MaxWidthWrapper className="">
+      <MaxWidthWrapper>
         <HeroSectionSlider />
         <About />
         <Categories />
         <NewArrivals />
         <Features />
       </MaxWidthWrapper>
+      <div className="flex">
+        <div className="h-[530px] w-[50%] relative">
+          <Image src="/sale.jpg" fill alt="sale" />
+        </div>
+        <div>
+          <p>SALE UP TO 35% OFF</p>
+          <p>HUNDREDS of New lower prices!</p>
+          <p>
+            Its more affordable than ever to give every room in your home a
+            stylish makeover
+          </p>
+          <ShopNowButton to="/shop" color="black" />
+        </div>
+      </div>
     </>
   );
 };
