34187276813211a82e4e136cd0a7da8777e5f658
Afridi <afridiwhatever@gmail.com>
2024-05-02 18:16:35 +0600

    newsletter section done, working on footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,8 @@ import Features from "@/components/Features";
 import Articles from "@/components/Articles";
 import ShopNowButton from "@/components/ShopNowButton";
 import Image from "next/image";
+import { MailIcon } from "../../public/icons/Icons";
+import Footer from "@/components/Footer";
 
 const HomePage = () => {
   return (
@@ -43,6 +45,32 @@ const HomePage = () => {
       <MaxWidthWrapper>
         <Articles />
       </MaxWidthWrapper>
+      {/* NewsLetter section */}
+      <div className="relative mt-10 lg:mt-20 py-36 px-8 bg-neutralGray overflow-hidden">
+        <h3 className="font-poppins text-3xl lg:text-5xl max-w-max mx-auto ">
+          Join Our Newsletter
+        </h3>
+        <p className="max-w-max mx-auto mt-2 mb-8  lg:text-lg">
+          Sign up for deals, new products and promotions
+        </p>
+
+        {/*TODO: make this a server action */}
+        <form className="relative max-w-max mx-auto py-2.5 border-b border-zinc-400">
+          <input
+            type="email"
+            className="placeholder-gray-500 pl-8 bg-transparent outline-none"
+            placeholder="Email address"
+          />
+          <button type="button">Signup</button>
+          <div className="absolute left-0 top-[50%] -translate-y-[50%]">
+            <MailIcon />
+          </div>
+        </form>
+        <div className="hidden lg:block absolute -left-[12%] -top-5 bottom-0 bg-[url('/bg/left-bg.png')] w-[25%] bg-center scale-110"></div>
+        <div className="hidden lg:block absolute -right-[12%] -top-5 bottom-0 bg-[url('/bg/right-bg.png')] w-[25%] bg-left bg-cover scale-125"></div>
+      </div>
+
+      <Footer />
     </>
   );
 };
---
+++ src/components/Footer.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Footer = () => {
+  return <div className="bg-black text-white h-36 w-full"></div>;
+};
+
+export default Footer;
