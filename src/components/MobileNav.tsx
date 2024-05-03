574191934d55f0b95353c013de1e00bf90314612
Afridi <afridiwhatever@gmail.com>
2024-05-03 07:17:17 +0600

    working on a better mobile nav

--- src/components/MobileNav.tsx
+++ src/components/MobileNav.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { cn } from "@/lib/utils";
 import Link from "next/link";
-import { X } from "lucide-react";
+import { X, Search } from "lucide-react";
+import Image from "next/image";
 
 const MobileNav = ({
   isMenuOpen,
@@ -13,33 +14,52 @@ const MobileNav = ({
   return (
     <div
       className={cn(
-        "bg-zinc-100 h-screen w-[45%] lg:hidden absolute inset-0 pt-24 transition-transform transform duration-300 -translate-x-full",
+        "bg-white h-screen w-[95%] lg:hidden absolute inset-0 p-8 transition-transform transform duration-300 -translate-x-full",
         {
           "translate-x-0": isMenuOpen,
         }
       )}
     >
-      <ul className="flex flex-col gap-10 max-w-max mx-auto">
-        <li>
-          <Link href="/">Home</Link>
-        </li>
-        <li>
-          <Link href="/">Shop</Link>
-        </li>
-        <li>
-          <Link href="/">Product</Link>
-        </li>
-        <li>
-          <Link href="/contact">Contact Us</Link>
-        </li>
-      </ul>
+      {/* menu items */}
+      <div className="space-y-4">
+        <div className="flex justify-between">
+          <Image src={"/3legant..png"} height={24} width={90} alt="logo" />
+          <X onClick={() => setIsMenuOpen(false)} className="h-6 w-6 " />
+        </div>
+        <div className="relative">
+          <input
+            type="text"
+            placeholder="Search"
+            className="pl-10 py-4 outline-none border border-black rounded-lg bg-transparent w-full"
+          />
+          <Search
+            className="h-7 w-7 absolute top-[50%] left-2 -translate-y-[50%]"
+            strokeWidth={1}
+          />
+        </div>
+        <ul className="space-y-4">
+          <li className="border-b py-4 border-b-[#E8ECEF]">
+            <Link href="/">Home</Link>
+          </li>
+          <li className="border-b py-4 border-b-[#E8ECEF]">
+            <Link href="/">Shop</Link>
+          </li>
+          <li className="border-b py-4 border-b-[#E8ECEF]">
+            <Link href="/">Product</Link>
+          </li>
+          <li className="border-b py-4 border-b-[#E8ECEF]">
+            <Link href="/contact">Contact Us</Link>
+          </li>
+        </ul>
+      </div>
 
-      <X
-        onClick={() => setIsMenuOpen(false)}
-        className="absolute top-5 right-5 h-6 w-6 border border-zinc-900 rounded-full"
-      />
+      {/* cart and stuff */}
+      <div></div>
     </div>
   );
 };
 
 export default MobileNav;
+
+{
+}
