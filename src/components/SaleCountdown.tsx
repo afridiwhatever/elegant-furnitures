98acb79ebfbf92160ce6fa712d71b957a75946ee
Afridi <afridiwhatever@gmail.com>
2024-05-07 05:14:20 +0600

    small bug fix - salecountdown comp (0 behind days)

--- src/components/SaleCountdown.tsx
+++ src/components/SaleCountdown.tsx
@@ -6,7 +6,9 @@ const renderer = ({ days, hours, minutes, seconds }: any) => {
   return (
     <div className="font-poppins text-3xl flex gap-4">
       <div>
-        <div className="bg-neutralGray px-3 py-3 rounded-sm">{"0" + days}</div>
+        <div className="bg-neutralGray px-3 py-3 rounded-sm">
+          {days >= 10 ? days : "0" + days}
+        </div>
         <p className="text-xs text-blackishGray max-w-max mx-auto">Days</p>
       </div>
       <div>
@@ -27,7 +29,7 @@ const renderer = ({ days, hours, minutes, seconds }: any) => {
 
 const SaleCountdown = () => {
   const saleEndDate = new Date();
-  saleEndDate.setDate(new Date().getDate() + 3);
+  saleEndDate.setDate(new Date().getDate() + 9);
   return (
     <>
       <Countdown date={saleEndDate} renderer={renderer} />
