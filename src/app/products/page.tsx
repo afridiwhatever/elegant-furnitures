d654dd1135781eaa6a10b7693b34a5e49e1f4c2a
Afridi <afridiwhatever@gmail.com>
2024-06-06 17:21:24 +0600

    added the icons and so

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,5 +1,14 @@
 import React from "react";
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+import {
+  SlidersHorizontal,
+  ChevronDown,
+  StretchHorizontal,
+  StretchVertical,
+  Grip,
+  Grid3X3,
+  LayoutGrid,
+} from "lucide-react";
 
 const Products = () => {
   return (
@@ -24,7 +33,35 @@ const Products = () => {
           </div>
 
           {/* filter and all */}
-          <div className="py-8 md:py-48"></div>
+          <div className="py-8 md:py-12font-[400] space-y-2">
+            <div className="flex justify-between items-center py-2 border-y">
+              <div className="flex items-center gap-2">
+                <SlidersHorizontal className="w-4 h-4" strokeWidth={3} />
+                <span className="block">Filter</span>
+              </div>
+              <div className="flex">
+                <div className="py-2 px-3 border">
+                  <Grip fill="black" strokeWidth={4} />
+                </div>
+                <div className="py-2 px-3 border">
+                  <LayoutGrid fill="black" />
+                </div>
+                <div className="py-2 px-3 border bg-neutralGray">
+                  <StretchVertical fill="black" />
+                </div>
+                <div className="py-2 px-3 border">
+                  <StretchHorizontal fill="black" />
+                </div>
+              </div>
+            </div>
+            <div className="flex justify-between items-center">
+              <span className="block font-semibold">All Rooms</span>
+              <div className="flex items-center gap-2">
+                Sort By
+                <ChevronDown />
+              </div>
+            </div>
+          </div>
         </MaxWidthWrapper>
       </section>
     </main>
